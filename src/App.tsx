ff671b2dffd2d17fda225b9d53c08fb5a00eb6a2
Nguyen Le Hoang <tonyhoangnguyen.work@gmail.com>
2026-06-22 16:59:47 +0700

    delete header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Nav } from '@/components/layout/Nav';
-import { Footer } from '@/components/layout/Footer';
 import { Hero, Marquee, Products, Roadmap, Trainers, Register } from '@/components/sections';
 import { AppTweaksPanel } from '@/components/tweaks/AppTweaksPanel';
 import { ScrollNav } from '@/components/ui/ScrollNav';
@@ -11,14 +9,12 @@ export function App() {
   return (
     <>
       <ScrollNav />
-      <Nav />
       <Hero tweaks={tweaks} />
       {tweaks.showMarquee && <Marquee />}
       <Products />
       <Roadmap />
       <Trainers tweaks={tweaks} />
       <Register tweaks={tweaks} />
-      <Footer />
       <AppTweaksPanel tweaks={tweaks} setTweak={setTweak} />
     </>
   );
